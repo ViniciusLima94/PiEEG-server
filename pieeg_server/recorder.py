a19ddb56dd55a86faf8e61bb2ddf8ac0dc11ea11
ViniciusLima94 <vinicius.lima.cordeiro@gmail.com>
2026-04-20 13:33:26 +0200

    added button pin  to registered signals - recorded with eeg

--- pieeg_server/recorder.py
+++ pieeg_server/recorder.py
@@ -80,7 +80,7 @@ class Recorder:
                     frame = await self._queue.get()
                     channels = frame.get("channels", [])
                     button = frame.get("button", [])
-                    writer.writerow([frame["t"], *channels, *button])
+                    writer.writerow([frame["t"], *channels, button])
                     self._frames_written += 1
 
                     # Flush every 250 frames (~1 second at 250 Hz)
